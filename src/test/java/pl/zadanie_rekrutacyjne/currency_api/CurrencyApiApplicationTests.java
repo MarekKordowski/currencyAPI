25ace448ed965d79ea3519bfbd53d33f15d376a2
MarekKordowski <m.kordowski21@gmail.com>
2022-02-10 15:00:22 +0100

    Initial commit

---
+++ src/test/java/pl/zadanie_rekrutacyjne/currency_api/CurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zadanie_rekrutacyjne.currency_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
